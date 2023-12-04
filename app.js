3d4b073d9ffad78ec0b48924d27aef1a2a4b3a97
mohanavamsi0614 <mohana.gangisetti@kalvium.community>
2023-12-04 14:01:30 +0530

    done

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ function main() {
        
         boxes[i].addEventListener("click",()=>{
             c++
-            boxes[i].disabled = true;
             boxes[i].style.color="#FAB201"
             if (ans=="X"){
                 ans="O"
@@ -18,38 +17,19 @@ function main() {
             }
             boxes[i].textContent=ans
             check()
-        })
-        
-    }
-   
-    
-    
+        })  
+    }  
 }
 main()
 function check() {
     if ((boxes[0].textContent==boxes[1].textContent && boxes[2].textContent==boxes[1].textContent && boxes[0].innerHTML!="")||boxes[3].textContent==boxes[4].textContent && boxes[5].textContent==boxes[4].textContent && boxes[3].innerHTML!=""||boxes[6].textContent==boxes[7].textContent && boxes[8].textContent==boxes[7].textContent && boxes[6].innerHTML!=""){
-        document.getElementById("result").style.visibility="visible"
-        document.getElementById("message").textContent=`${ans} has won the game`
-        document.getElementById("button").onclick=()=>{
-            document.getElementById("result").style.visibility="hidden" 
-            location.reload("./app.js")
-        }
+        result()
     }
     else if(boxes[0].textContent==boxes[3].textContent&&boxes[6].textContent==boxes[3].textContent && boxes[3].innerHTML!="" || boxes[1].textContent==boxes[4].textContent && boxes[7].textContent==boxes[1].textContent && boxes[1].innerHTML!=""||boxes[2].textContent==boxes[5].textContent && boxes[8].textContent==boxes[2].textContent && boxes[8].innerHTML!=""){
-        document.getElementById("result").style.visibility="visible"
-        document.getElementById("message").textContent=`${ans} has won the game`
-        document.getElementById("button").onclick=()=>{
-            document.getElementById("result").style.visibility="hidden" 
-            location.reload("./app.js")
-        }
+        result()
     }
     else if(boxes[0].textContent==boxes[4].textContent && boxes[8].textContent==boxes[4].textContent && boxes[4].innerHTML!=""|| boxes[6].textContent==boxes[4].textContent&& boxes[2].textContent==boxes[6].textContent  && boxes[6].innerHTML!=""){
-        document.getElementById("result").style.visibility="visible"
-        document.getElementById("message").textContent=`${ans} has won the game`
-        document.getElementById("button").onclick=()=>{
-            document.getElementById("result").style.visibility="hidden" 
-            location.reload("./app.js")
-        }
+        result()
     }
 
     if (c==9){
@@ -60,4 +40,12 @@ function check() {
             location.reload("./app.js")
         }
     }
+}
+function result() {
+    document.getElementById("result").style.visibility="visible"
+        document.getElementById("message").textContent=`${ans} has won the game`
+        document.getElementById("button").onclick=()=>{
+            document.getElementById("result").style.visibility="hidden" 
+            location.reload("./app.js")
+        }
 }
